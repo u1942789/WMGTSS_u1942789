cf87b8c040aab23abc0b86325928c39e8a3bd43d
u1942789 <u1942789@live.warwick.ac.uk>
2022-02-05 09:11:09 +0000

    HTML and CSS files restarted to adhere to agile development. Files being pushed due to IDE bug.

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("qa_home_template.html")
 
 
