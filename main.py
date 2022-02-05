e61913a62f1a5d14d2e755af13eb34df48e4bb17
u1942789 <u1942789@live.warwick.ac.uk>
2022-02-05 17:42:03 +0000

    Some HTML updated. Some data being passed from Python to the HTML page.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+questions = ["test", "best"]
 
 @app.route("/")
-def home():
-    return render_template("qa_home_template.html")
+def test():
+    return redirect(url_for("qa_home"))
+
+
+@app.route("/qa_home")
+def qa_home():
+    return render_template("qa_home_template.html", questions=questions)
 
 
 if __name__ == "__main__":
