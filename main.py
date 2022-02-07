55b85c46c63c7237fc3b2a80b38330597ea756b5
u1942789 <u1942789@live.warwick.ac.uk>
2022-02-07 01:04:54 +0000

    Before we can work on answers and deletions, we need to make sure we know who the user is. This is because only tutors can answer questions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 
 app = Flask(__name__)
 
